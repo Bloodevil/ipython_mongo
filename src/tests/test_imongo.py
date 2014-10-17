b32d587a042885939cf024c2c34a5e561f8d2418
Dante Alighieri <dante@Dantes-MacBook-Pro.local>
2014-10-17 20:34:36 +0900

    #4 add test module
     add test module using get_ipython().
     - init mongo data
     - teardown
     - setup

---
+++ src/tests/test_imongo.py
@@ -0,0 +1,16 @@
+from imongo.imongo import MongoDB
+
+ip = get_ipython()
+
+def setup():
+    imongo = MongoDB(shell=ip)
+    ip.register_magic(imongo)
+
+def _init():
+    ip.run_line_magic('mongo_connect')
+    ip.run_line_magic('insert', "ipython.test {'test': 1}")
+
+def _teardown():
+    ip.run_cell("conn = %mongo_connect")
+    ip.runcode("conn.ipython.test.drop()")
+    #ip.run_line_magic('delete', "ipython.test")
